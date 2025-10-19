65b3e69199fdda275cd9c5f185b5d26a40a8bb40
wannurizzatiwanabdazizktb-arch <wannurizzatiwanabdazizktb@gmail.com>
2025-10-19 15:00:05 +0800

    Update studentSurvey.py

--- studentSurvey.py
+++ studentSurvey.py
@@ -8,18 +8,18 @@ st.header("Student Survey")
 # Load CSV from GitHub
 url = "https://raw.githubusercontent.com/wannurizzatiwanabdazizktb-arch/EC2024/refs/heads/main/arts_faculty_data.csv"
 
-try:
-    arts_df = pd.read_csv(url)
-    st.dataframe(arts_df.head())
-except Exception as e:
-    st.error(f"An error occurred while reading the CSV file: {e}")
-
 col1, col2, col3, col4 = st.columns(4)
     
 col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
 col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
 col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
 col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+
+try:
+    arts_df = pd.read_csv(url)
+    st.dataframe(arts_df.head())
+except Exception as e:
+    st.error(f"An error occurred while reading the CSV file: {e}")
 
 # Pie chart: Arts Program distribution
 program_counts = arts_df['Arts Program'].value_counts().reset_index()
