e805d1ccaa9f3d6113bb55ef23d857a136a5a4b1
wannurizzatiwanabdazizktb-arch <wannurizzatiwanabdazizktb@gmail.com>
2025-10-19 14:59:20 +0800

    Update studentSurvey.py

--- studentSurvey.py
+++ studentSurvey.py
@@ -16,10 +16,10 @@ except Exception as e:
 
 col1, col2, col3, col4 = st.columns(4)
     
-    col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
-    col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
-    col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
-    col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
 
 # Pie chart: Arts Program distribution
 program_counts = arts_df['Arts Program'].value_counts().reset_index()
