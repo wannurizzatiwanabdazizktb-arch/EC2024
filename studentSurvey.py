f46c05f35a6e5d4af6d5d35e4f6d6574a1e6bded
wannurizzatiwanabdazizktb-arch <wannurizzatiwanabdazizktb@gmail.com>
2025-10-19 14:44:52 +0800

    Update studentSurvey.py

--- studentSurvey.py
+++ studentSurvey.py
@@ -8,6 +8,13 @@ st.header("Student Survey")
 # Load CSV from GitHub
 url = "https://raw.githubusercontent.com/wannurizzatiwanabdazizktb-arch/EC2024/refs/heads/main/arts_faculty_data.csv"
 
+col1, col2, col3, col4 = st.columns(4)
+    
+    col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+    col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+    col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+    col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+
 try:
     arts_df = pd.read_csv(url)
     st.dataframe(arts_df.head())
